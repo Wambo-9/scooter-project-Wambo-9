c149742683c652430215b847745154c0c7af480a
Wambo-9 <irene.wambui@directlinegroup.co.uk>
2022-08-05 17:33:44 +0100

    wrote some code on the app and the scooter /user js

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,13 @@
 class Scooter {
-  // scooter code here
+  constructor (scooterId, typeOfBike){
+    this.scooterId = Math.trunc(Math.random() * 10000000)
+  }
+rent(userName){
+  this.currentUser = userName; 
+}
+dock (){
+  this.currentUser = ' '
+}
 }
 
 module.exports = Scooter;
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,43 @@ const Scooter = require('./Scooter');
 
 class ScooterApp {
   // ScooterApp code here
+  constructor( ){
+    this.newCustomers = [ ]; 
+  }
 }
+  new(username, password) {
+    //if customer already exists  in newcustomer array, return a message saying already registered 
+    for(let i = 0; i < this.newCustomers.length; i++){
+      if (this.newCustomers[i]. username === username){
+        console.log('User already exists')
+        return; 
+      } 
+    }
+  }
+    let newUser = new User (username, password); 
+    this.newCustomers.push(newUser); 
+
+
+  existing(username, password){
+//log existing customer in
+for(let i = 0; i < this.newCustomers.length; i++){
+  if (this.newCustomers[i]. username === username && this.newCustomers [i]. password === password){
+    console.log('Log in succesful')
+    return; 
+  }
+}
+addScooter () {
+  //registers a new scooter for the fleet
+  let newScooter = new scooter();
+  Scooter.fleet.push(newScooter); 
+  return newScooter; 
+}
+  }
+let myApp = new ScooterApp(); 
+myApp.newCustomers('irene', 'password'); 
+myApp.existing('Irene', 'password'); 
+myApp.addScooter(); 
+myApp.addScooter(); 
+Scooter.rent('jordan'); 
 
 module.exports = ScooterApp;
